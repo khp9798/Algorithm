3539e12a66fa5808479e3241d599afe5eef842af
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-08-19 23:39:14 +0900

    [Bronze V] Title: 새싹, Time: 96 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,17 @@
+
+
+public class Main {
+
+	// a부터 z까지 26
+	public static void main(String[] args) {
+		System.out.println("         ,r'\"7\n"
+				+ "r`-_   ,'  ,/\n"
+				+ " \\. \". L_r'\n"
+				+ "   `~\\/\n"
+				+ "      |\n"
+				+ "      |");
+		
+		
+	}
+
+}
