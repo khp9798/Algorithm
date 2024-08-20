bd351443d5ef589f53fae96dfa38fa8794696761
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-08-20 19:24:57 +0900

    [Bronze I] Title: 소인수분해, Time: 132 ms, Memory: 14660 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -4,7 +4,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-
 public class Main {
 
 	
@@ -14,12 +13,10 @@ public class Main {
 		
 		int n = Integer.parseInt(br.readLine());
 		
-		prime = new boolean[n+1];
-		prime();
-//		System.out.println(Arrays.toString(prime));
 		
-		for(int i=2; i<prime.length; i++) {
-			if(!prime[i] && n%i==0) {
+		
+		for(int i=2; i<n+1; i++) {
+			if(n%i==0) {
 				while(n%i==0) {
 					System.out.println(i);
 					n/=i;
@@ -28,15 +25,6 @@ public class Main {
 		}
 	}
 	
-	static void prime() {
-		prime[0] = true;
-		prime[1] = true;
-		
-		for(int i=2; i<Math.sqrt(prime.length-1); i++) {
-			for(int j=i*i; j<=prime.length-1; j+=i) {
-				prime[j] = true;
-			}
-		}
-	}
+	
 
 }
