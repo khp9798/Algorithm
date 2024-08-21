8edf0acd49cd3377d8a4f04372014de9c00ec4b9
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-08-21 13:51:15 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 168 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		int d = Integer.parseInt(st.nextToken());
+		int e = Integer.parseInt(st.nextToken());
+		int f = Integer.parseInt(st.nextToken());
+
+		int answerx = 0;
+		int answery = 0;
+		for (int x = -999; x <= 999; x++) {
+			for (int y = -999; y <= 999; y++) {
+				if (a * x + b * y == c && d * x + e * y == f) {
+					answerx = x;
+					answery = y;
+					break;
+				}
+			}
+		}
+
+		System.out.println(answerx + " " + answery);
+
+	}
+
+}
