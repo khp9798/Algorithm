a67b7d8ec9d51a3b0f74bf3a7e358aae10479621
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-08-28 01:24:41 +0900

    [Silver II] Title: 이항 계수 2, Time: 320 ms, Memory: 85368 KB -BaekjoonHub

---
+++ 백준/Silver/11051. 이항 계수 2/이항 계수 2.java
@@ -0,0 +1,40 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	static int max = 1000;
+	static BigInteger [][] dp = new BigInteger[max+1][max+1];
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		
+		int K = sc.nextInt();
+
+		
+		comb_dp();
+		
+		BigInteger res = new BigInteger("10007");
+		System.out.println(dp[N][K].mod(res));
+	}
+
+
+	static void comb_dp() {
+		for(int i=0; i<dp.length; i++) {
+			dp[i][0] = BigInteger.ONE;
+			dp[i][i] = BigInteger.ONE;
+		}
+		
+		
+		for(int i=1; i<=max; i++) {
+			for(int j=1; j<i; j++) {
+				dp[i][j] = dp[i-1][j-1].add(dp[i-1][j]);
+			}
+		}
+		
+	}
+
+}
