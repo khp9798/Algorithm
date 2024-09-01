f21e455e6c7bc6cd11b00a3f301de6fd6e847f4d
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-01 13:08:49 +0900

    [Silver IV] Title: 알고리즘 수업 - 병합 정렬 2, Time: 2788 ms, Memory: 131340 KB -BaekjoonHub

---
+++ 백준/Silver/24061. 알고리즘 수업 － 병합 정렬 2/알고리즘 수업 － 병합 정렬 2.java
@@ -0,0 +1,86 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] A;
+	static int[] tmp;
+	static int K;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+
+		A = new int[N];
+		tmp = new int[A.length];
+		
+		K = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+
+		MergeSort(0, N - 1);
+
+		if(count<K) {
+			System.out.println(-1);
+		}
+
+	}
+	
+	static int count = 0;
+	static int [] res;
+
+	private static void MergeSort(int left, int right) {
+		if (left < right) {
+			int mid = (left + right) / 2;
+			MergeSort(left, mid);
+			MergeSort(mid + 1, right);
+			Merge(left, mid, right);
+		}
+
+	}
+
+	private static void Merge(int left, int mid, int right) {
+		int L = left, R = mid + 1, idx = left;
+
+		while (L <= mid && R <= right) {
+			if (A[L] < A[R]) {
+				tmp[idx++] = A[L++];
+			} else {
+				tmp[idx++] = A[R++];
+			}
+		}
+
+		while (L <= mid) {
+			tmp[idx++] = A[L++];
+		}
+
+		while (R <= right) {
+			tmp[idx++] = A[R++];
+		}
+
+		for (int i = left; i <= right; i++) {
+			A[i] = tmp[i];
+			count++;
+			if(count==K) {
+				for(int j=0; j<A.length; j++) {
+					System.out.print(A[j]+" ");
+				}
+				return;
+			}
+			
+			
+		}
+	}
+
+}
