e237ffbb273ea575ae67e5c109ae70c7d528e506
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-01 12:57:16 +0900

    [Silver III] Title: 알고리즘 수업 - 병합 정렬 1, Time: 1060 ms, Memory: 467128 KB -BaekjoonHub

---
+++ 백준/Silver/24060. 알고리즘 수업 － 병합 정렬 1/알고리즘 수업 － 병합 정렬 1.java
@@ -0,0 +1,75 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static int[] A;
+	static int[] tmp;
+	static List<Integer> KList = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+
+		A = new int[N];
+		tmp = new int[A.length];
+		int K = Integer.parseInt(st.nextToken());
+
+		st = new StringTokenizer(br.readLine());
+		for (int i = 0; i < N; i++) {
+			A[i] = Integer.parseInt(st.nextToken());
+		}
+
+		MergeSort(0, N - 1);
+
+		try {
+			System.out.println(KList.get(K - 1));
+		} catch (IndexOutOfBoundsException e) {
+			System.out.println(-1);
+		}
+
+	}
+
+	private static void MergeSort(int left, int right) {
+		if (left < right) {
+			int mid = (left + right) / 2;
+			MergeSort(left, mid);
+			MergeSort(mid + 1, right);
+			Merge(left, mid, right);
+		}
+
+	}
+
+	private static void Merge(int left, int mid, int right) {
+		int L = left, R = mid + 1, idx = left;
+
+		while (L <= mid && R <= right) {
+			if (A[L] < A[R]) {
+				tmp[idx++] = A[L++];
+			} else {
+				tmp[idx++] = A[R++];
+			}
+		}
+
+		while (L <= mid) {
+			tmp[idx++] = A[L++];
+		}
+
+		while (R <= right) {
+			tmp[idx++] = A[R++];
+		}
+
+		for (int i = left; i <= right; i++) {
+			A[i] = tmp[i];
+			KList.add(tmp[i]);
+		}
+	}
+
+}
