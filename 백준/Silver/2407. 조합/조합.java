66601080ae2eee15263ab01ed6a7b58d9ebe6766
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-01 20:00:49 +0900

    [Silver III] Title: 조합, Time: 104 ms, Memory: 14696 KB -BaekjoonHub

--- 백준/Silver/2407. 조합/조합.java
+++ 백준/Silver/2407. 조합/조합.java
@@ -1,37 +1,50 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.math.BigInteger;
+import java.util.StringTokenizer;
 
 public class Main {
-
+	
 	static int N;
 	static int M;
 	static int max = 100;
-	static BigInteger [][] dp = new BigInteger[max+1][max+1];
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-
-		N = sc.nextInt();
-		M = sc.nextInt();
-
-		// nCm
-		comb_dp();
-		System.out.println(dp[N][M]);
-		sc.close(); // Scanner 닫기
-	}
+	static BigInteger [][] arr = new BigInteger[max+1][max+1];
 	
-	static void comb_dp() {
-		// 초기화 부분에서 BigInteger.ONE으로 초기화
-		for(int i = 0; i <= max; i++) {
-			dp[i][0] = BigInteger.ONE;
-			dp[i][i] = BigInteger.ONE;
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		
+		M = Integer.parseInt(st.nextToken());
+		
+		comb_dp();
+
+		System.out.println(arr[N][M]);
+	}
+
+	private static void comb_dp() {
+		//nC0 == 1 nCn == 1
+		for(int i=0; i<=max; i++) {
+			arr[i][0] = new BigInteger("1");
+			arr[i][i] = new BigInteger("1");
 		}
 		
-		for(int i = 1; i <= max; i++) {
-			for(int j = 1; j < i; j++) {
-				// BigInteger의 더하기 연산은 add() 메서드를 사용해야 함
-				dp[i][j] = dp[i-1][j-1].add(dp[i-1][j]);
+		
+		
+		//nCr = n-1Cr-1 + n-1Cr
+		
+		for(int i=1; i<=max; i++) {
+			for(int j=1; j<i; j++) {
+				arr[i][j] = arr[i-1][j-1].add(arr[i-1][j]);
 			}
 		}
+		
 	}
+
+	
+
 }
