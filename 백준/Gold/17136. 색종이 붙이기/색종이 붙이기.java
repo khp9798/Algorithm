bc49828f8b6ed64e72a11e62d76940a5a60b309a
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-04 21:36:04 +0900

    [Gold II] Title: 색종이 붙이기, Time: 332 ms, Memory: 27336 KB -BaekjoonHub

--- 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
+++ 백준/Gold/17136. 색종이 붙이기/색종이 붙이기.java
@@ -1,10 +1,10 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 
 	static int[][] map = new int[10][10];
-	static int[] cnt = new int[6];
-	static int res = Integer.MAX_VALUE;
+	static int [] cnt = new int[6];
 
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -15,53 +15,75 @@ public class Main {
 			}
 		}
 
-		dfs(0, 0);
+		dfs(0,0);
+		
+		
+		//System.out.println(Arrays.toString(cnt));
+		
+		System.out.println(res==Integer.MAX_VALUE? -1:res);
 
-		System.out.println(res == Integer.MAX_VALUE ? -1 : res);
 	}
+	
 
+	static int res = Integer.MAX_VALUE;
 	static void dfs(int r, int c) {
-		if (r >= 10) {  // 행의 끝까지 탐색했을 경우
-			res = Math.min(res, cnt[1] + cnt[2] + cnt[3] + cnt[4] + cnt[5]);
+		if(r>9) {
+			int sum=0;
+			for(int i=1; i<cnt.length; i++) {
+				sum+=cnt[i];
+			}
+			res = Math.min(res, sum);
 			return;
 		}
-
-		if (c >= 10) {  // 열의 끝에 도달하면 다음 행으로 이동
-			dfs(r + 1, 0);
+		if(c>9) {
+			dfs(r+1,0);
 			return;
 		}
+		
+		if(map[r][c]!=1) {
+			dfs(r,c+1);
+			
+		}
+		else {
+			for(int size=5; size>=1; size--) {
+				if(cnt[size]<5 && check(r,c,size)) {
 
-		if (map[r][c] == 1) {  // 현재 위치가 1인 경우 색종이를 붙일 수 있는지 확인
-			for (int size = 5; size >= 1; size--) {
-				if (cnt[size] < 5 && check(r, c, size)) {
-					paper(r, c, size, 0);
+					paper(r,c,size,0);
 					cnt[size]++;
-					dfs(r, c + 1);
-					paper(r, c, size, 1);
+					
+					dfs(r,c+1);
+					
+					paper(r,c,size,1);
 					cnt[size]--;
 				}
 			}
-		} else {  // 현재 위치가 1이 아니면 다음 칸으로 이동
-			dfs(r, c + 1);
 		}
+		
+		
+		
+		
 	}
-
-	static boolean check(int r, int c, int size) {
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				if (r + i >= 10 || c + j >= 10 || map[r + i][c + j] != 1) {
-					return false;
-				}
+	
+	static void paper(int r, int c, int size, int num) {
+		for (int row = r; row < r + size; row++) {
+			for (int col = c; col < c + size; col++) {
+				if (row >= 10 || col >= 10)
+					continue;
+				map[row][col] = num;
 			}
 		}
+	}
+	
+	
+	static boolean check(int r, int c, int size) {
+		for (int row = r; row < r + size; row++) {
+			for (int col = c; col < c + size; col++) {
+				if (row >= 10 || col >= 10 || map[row][col] != 1)
+					return false;
+			}
+		}
+
 		return true;
 	}
 
-	static void paper(int r, int c, int size, int num) {
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				map[r + i][c + j] = num;
-			}
-		}
-	}
 }
