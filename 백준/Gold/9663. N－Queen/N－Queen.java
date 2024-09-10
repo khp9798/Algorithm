a65ba3777666065b6f0026ea4afd36ab8d696246
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-10 13:26:37 +0900

    [Gold IV] Title: N-Queen, Time: 3720 ms, Memory: 18036 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,74 +1,63 @@
-
-
 import java.util.Scanner;
 
-public class Main {
-
+class Main{
 	
-	//N이 주어졌을 때 NxN 체스판에서 퀸을 놓는 방법의 수 구하기
+	
 	static int N;
-	
-	
-	//1차원 배열로 풀어보기
 	static int [] board;
-	
 	static boolean [] visited;
 	
-	static int res = 0;
-	
-	public static void main(String[] args) {
+	public static void main(String [] args) {
+		
 		Scanner sc = new Scanner(System.in);
+		
 		
 		N = sc.nextInt();
 		
+		
+		//board의 인덱스가 row, 값이 col
 		board = new int[N];
+		
 		visited = new boolean[N];
 		
-		//0행부터 탐색 시작
-		Queen(0);
+		dfs(0);
 		
 		System.out.println(res);
 	}
 
-	private static void Queen(int row) {
-		//행 끝까지 갔다면 경우의 수++
-		if(row == N) {
-			res ++;
+	static int res=0;
+	private static void dfs(int r) {
+		if(r==N) {
+			res++;
 			return;
 		}
 		
 		
-		//지금 행에서 가능한 열 찾기
-		for(int col=0; col<N; col++) {
-			//퀸이 공격할 수 없다면
-			if(NoQueenAttack(row,col)) {
-				//퀸 위치 저장
-				board[row] = col;
-				//해당 열의 퀸이 있다 체크
-				visited[col] = true;
-				//다음 행으로 이동
-				Queen(row+1);
-				//가다가 아닌 경우일 수 있으므로 백트래킹을 위해 체크 풀기
-				visited[col] = false;
+		for(int i=0; i<N; i++) {
+			if(checkQ(r,i)) {
+				board[r] = i;
+				visited[i] = true;
+				dfs(r+1);	
+				board[r] = 0;
+				visited[i] = false;
 			}
 		}
 		
 	}
-
-	private static boolean NoQueenAttack(int row, int col) {
-		//이미 그 열에 퀸이 있다면 false;
-		if(visited[col]) return false;
+	
+	
+	static boolean checkQ(int r, int c) {
+		if (visited[c]) return false;
 		
-		//지금까지의 퀸에 대각선 상에 없어야 한다.
-		//대각선에 있다는 것은 0,0 2,2   row1-row2의 절댓값과 col1-col2의 절댓값이 같다.
-		for(int r=0; r<row; r++) {
-			if(Math.abs(row-r) == Math.abs(col-board[r])) return false;
+		
+		for(int i=0; i<r; i++) {
+			if( Math.abs(i-r) == Math.abs(board[i]-c)) {
+				return false;
+			}
 		}
 		
 		return true;
 	}
-	
-	
-	
 
+	
 }
