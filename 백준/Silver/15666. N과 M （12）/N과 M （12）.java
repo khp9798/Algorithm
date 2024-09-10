b9a9877714e67d6fb0110088f3c58ad9023c2464
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-10 15:09:55 +0900

    [Silver II] Title: N과 M (12), Time: 220 ms, Memory: 19112 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,69 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.Set;
+
+class Main {
+
+	static int N;
+	static int M;
+	static int[][] visited;
+	static boolean[] select;
+	static int[] sel;
+	static int[] num;
+	static StringBuilder sb = new StringBuilder();
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		N = sc.nextInt();
+
+		M = sc.nextInt();
+
+		num = new int[N];
+		
+
+		for (int i = 0; i < N; i++) {
+			num[i] = sc.nextInt();
+
+		}
+
+		
+		Arrays.sort(num);
+
+
+		sel = new int[M];
+		select = new boolean[N];
+
+
+		dfs(0,0);
+
+		System.out.println(sb.toString());
+
+	}
+
+	private static void dfs(int idx,int depth) {
+		if (depth == M) {
+			for (int i : sel) {
+				sb.append(i + " ");
+			}
+			sb.append("\n");
+
+			return;
+		}
+
+		int xx=0;
+		for (int i = idx; i < N; i++) {
+			if(num[i]!=xx) {
+				
+				xx = num[i];
+				sel[depth] = num[i];
+				dfs(i,depth + 1);				
+				
+			}
+
+		}
+
+	}
+
+}
