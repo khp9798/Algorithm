f11174c86a794b461c16d805c39af2c4de04d314
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-24 22:18:41 +0900

    [Silver V] Title: 거스름돈, Time: 100 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -1,30 +1,28 @@
-import java.io.*;
-import java.util.Arrays;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-public class Main {
-
-	static int[] dp;
-
+class Main {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 		int N = Integer.parseInt(br.readLine());
 
-		dp = new int[100001];
+		int[] dp = new int[100001];
 
-		Arrays.fill(dp, Integer.MAX_VALUE);
+		int MAX = Integer.MAX_VALUE;
 
+		dp[0] = MAX;
+		dp[1] = MAX;
 		dp[2] = 1;
+		dp[3] = MAX;
 		dp[4] = 2;
 		dp[5] = 1;
-		
-		
-		for(int i=6; i<=N; i++) {
-			dp[i] = Math.min(dp[i-5], dp[i-2])+1;
-		}
-		
-		
-		System.out.println(dp[N] == Integer.MAX_VALUE ? -1 : dp[N]);
-	}
 
+		for (int i = 6; i <= N; i++) {
+			dp[i] = Math.min(dp[i - 2], dp[i - 5]) + 1;
+		}
+
+		System.out.println(dp[N] == MAX ? -1 : dp[N]);
+	}
 }
