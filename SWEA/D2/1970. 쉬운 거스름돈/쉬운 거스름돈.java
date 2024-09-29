a81f96f13f2d648b96009fb9ec92bd67a40a8fad
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-29 20:36:54 +0900

    [D2] Title: 쉬운 거스름돈, Time: 162 ms, Memory: 24,776 KB -BaekjoonHub

--- SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
+++ SWEA/D2/1970. 쉬운 거스름돈/쉬운 거스름돈.java
@@ -1,53 +1,55 @@
 import java.util.Arrays;
 import java.util.Scanner;
- 
+
 class Solution{
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-         
-        int T= sc.nextInt();
-         
-        int [] coins = new int[] {1,10,50,100,500,1000,5000,10000,50000};
-         
-        for(int t=1; t<=T; t++) {
-            int N = sc.nextInt();
-            System.out.println("#"+t);
-             
-             
-            int [] count = new int[coins.length];
-             
-            int [] p = new int[N+1];
-            int [] dp = new int[N+1];
-            Arrays.fill(dp, Integer.MAX_VALUE);
-            dp[0] = 0;
-             
-            for(int i = 0; i<coins.length; i++) {
-                for(int j=coins[i]; j<=N; j++) {
-                    if(dp[j-coins[i]] != Integer.MAX_VALUE) {
-                        dp[j] = Math.min(dp[j], dp[j-coins[i]]+1);
-                        if (dp[j] == dp[j-coins[i]]+1) {
-                            p[j] = j-coins[i];
-                        }
-                    }
-                }
-            }
-             
-//            System.out.println(Arrays.toString(dp));
-             
-            while(N>0) {
-                int money = N - p[N];
-                for(int i=0; i<coins.length; i++) {
-                    if(coins[i]==money) count[i]++;
-                }
-                N -= money;
-            }
-             
-             
-            for(int i=count.length-1; i>=1; i--) {
-                System.out.print(count[i]+" ");
-            }
-             
-            System.out.println();
-        }
-    }
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int T= sc.nextInt();
+		
+		for(int t=1; t<=T; t++) {
+			System.out.println("#"+t);
+			int N = sc.nextInt();
+			
+			N /= 10;
+			
+			int [] dp = new int[N+1];
+			Arrays.fill(dp, Integer.MAX_VALUE);
+			dp[0]=0;
+			
+			int [] coins = {1,5,10,50,100,500,1000,5000};
+			int [] p = new int[N+1];
+			
+			for (int coin : coins) {
+				for(int i=coin; i<=N; i++) {
+					if(dp[i-coin]!=Integer.MAX_VALUE) {
+						dp[i] = Math.min(dp[i], dp[i-coin]+1);
+						p[i] = coin;
+					}
+				}
+			}
+			
+//			System.out.println(Arrays.toString(p));
+			int [] count=new int[coins.length];
+			
+			
+			while(N>0) {
+				for(int j=0; j<coins.length; j++) {
+					if(p[N]==coins[j]) {
+						count[j]++;
+					}
+				}
+				N -= p[N];
+			}
+				
+			
+			
+			
+			for(int i=count.length-1; i>=0; i--) {
+				System.out.print(count[i]+" ");
+			}
+			System.out.println();
+			
+		}
+	}
 }
