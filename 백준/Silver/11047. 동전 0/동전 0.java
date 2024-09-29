b2aa34bf039f921cea5aff4a577cc62459f07317
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-09-29 18:20:04 +0900

    [Silver IV] Title: 동전 0, Time: 1884 ms, Memory: 400472 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,38 +1,35 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
-class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+public class Main {
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-
-		int[] coins = new int[N];
-		for (int i = 0; i < N; i++) {
-			coins[i] = Integer.parseInt(br.readLine());
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in)
+;
+		int N = sc.nextInt();
+		int K = sc.nextInt();
+		
+		int [] dp = new int[K+1];
+		
+		int [] coins = new int[N];
+		
+		for(int i=0; i<N; i++) {
+			coins[i] = sc.nextInt();
 		}
-
-		int[] dp = new int[K + 1];
+		
 		Arrays.fill(dp, Integer.MAX_VALUE);
-		dp[0] = 0;
-
+		dp[0]=0;
+		
 		for (int coin : coins) {
-			for (int i = coin; i <= K; i++) {
-				if (dp[i - coin] != Integer.MAX_VALUE) {
-					dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+			for(int i=coin; i<=K; i++) {
+				if(dp[i-coin]!=Integer.MAX_VALUE) {
+					dp[i] = Math.min(dp[i], dp[i-coin])+1;
 				}
 			}
 		}
-
 		
-
+		
 		System.out.println(dp[K]);
-
 	}
+
 }
