3c5cde61d947a34f9a77b57084e7686745ed2f78
khp9798 <121469544+khp9798@users.noreply.github.com>
2024-10-01 12:09:49 +0900

    [Silver III] Title: 1, Time: 164 ms, Memory: 17588 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+        while(sc.hasNextInt()){
+            int N = sc.nextInt();
+		
+		long num=1;
+		int count=1;
+		while(true) {
+			if(num%N==0) {
+				System.out.println(count);
+				break;
+			}
+			else {
+				count++;
+				num = num*10+1;
+				num%=N;
+			}
+		}
+        }
+		
+	}
+
+}
