1d3feead37b9cc0118555206869babe8d1857cb3
khp9798 <121469544+khp9798@users.noreply.github.com>
2025-03-10 22:37:50 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14592 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,103 +1,105 @@
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
-	
-	static int [][] map;
-	
-	//상하좌우
-	static int [] dr = {-1,1,0,0};
-	static int [] dc = {0,0,-1,1};
-	
-	//행의 길이
-	static int N;
-	
-	//열의 길이
-	static int M;
-	
-	
-	static int[][] miro;
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		//행의 길이
-		N = Integer.parseInt(st.nextToken());
-		
-		//열의 길이
-		M = Integer.parseInt(st.nextToken());
-		
-		map = new int[N][M];
-		
-		//miro 카운팅 배열을 만들어서 도착점에는 지금까지 얼마나 걸렸는지 나오게
-		miro = new int[N][M];
-		
-		//map 내용 입력 받기
-		for(int r=0; r<N; r++) {
-			String[] str = br.readLine().split("");
-			for(int c=0; c<M; c++) {
-				map[r][c] = Integer.parseInt(str[c]);
-			}
-		}
-		
-		
-		//최소거리를 구하기 때문에 bfs를 이용해야함.
-		bfs(0,0);
-		
-		
-		System.out.println(miro[N-1][M-1]);
+    static int N;
+    static int M;
 
-	}
-	
-	static class point {
-		int r;
-		int c;
-		
-		
-		public point(int r, int c) {
-			super();
-			this.r = r;
-			this.c = c;
-		}
-		
-	}
-	
-	static void bfs(int r, int c) {
-		//bfs이기 때문에 queue 사용
-		Queue<point> queue = new LinkedList<>();
-		queue.add(new point(r, c));
-		miro[r][c] = 1;
-		
-		while(!queue.isEmpty()) {
-			point p = queue.poll();
-			
-			for(int d=0; d<4; d++) {
-				int nr = p.r + dr[d];
-				int nc = p.c + dc[d];
-				
-				//범위를 벗어나면 queue에다 안넣음
-				if(0>nr || nr>=N || 0>nc || nc>=M) {
-					continue;
-				}
-				
-				//이미 방문 했거나 갈 수 없는 길이면 queue에 안넣음
-				if(miro[nr][nc]!=0 || map[nr][nc] ==0) {
-					continue;
-				}
-				
-				queue.add(new point(nr,nc));
-				miro[nr][nc] = miro[p.r][p.c] +1;
-			}
-		}
-		
-		
-		
-	}
+    static int [][] map;
+
+    static boolean [][] visited;
+
+    static int result = 0;
+
+    static class node{
+        int r;
+        int c;
+        int count;
+
+
+        node(int r, int c, int count){
+            this.r = r;
+            this.c = c;
+            this.count = count;
+        }
+    }
+
+
+    static int [] dr = {-1,1,0,0};
+    static int [] dc = {0,0,-1,1};
+
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+
+        map = new int[N][M];
+
+        visited = new boolean[N][M];
+
+
+        for(int i=0; i<N; i++){
+            String str = br.readLine();
+
+            for(int j=0; j<M; j++){
+                map[i][j] = str.charAt(j)-'0';
+            }
+        }
+
+
+        bfs(0,0);
+
+
+    }
+
+
+    public static void bfs(int r, int c){
+        Queue<node> queue = new LinkedList<>();
+        queue.add(new node(r,c,1));
+        visited[r][c] = true;
+
+        while(!queue.isEmpty()){
+            node node = queue.poll();
+
+            int row = node.r;
+            int cul = node.c;
+            int count = node.count;
+
+            if(row == N-1 && cul == M-1){
+                result = node.count;
+                System.out.println(result);
+                break;
+            }
+
+
+            for(int d=0; d<4; d++){
+                int nr = row+dr[d];
+                int nc = cul+dc[d];
+
+                if(nr<0 || nr>=N || nc<0 || nc>=M)
+                    continue;
+
+                if(visited[nr][nc])
+                    continue;
+
+                if(map[nr][nc]!=1)
+                    continue;
+
+                queue.add(new node(nr,nc,count+1));
+                visited[nr][nc] = true;
+            }
+
+
+        }
+
+    }
+
+
+    
 }
