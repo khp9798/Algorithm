d619001975f3dfe166e9d1f1e97aa332bc05a4c7
khp9798 <121469544+khp9798@users.noreply.github.com>
2025-03-10 23:24:19 +0900

    [Silver III] Title: 바이러스, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,62 +1,52 @@
+import java.io.*;
+import java.util.*;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+public class Main{
 
-public class Main {
+    static int N;
+    static int M;
 
-	static int Node;
-	static int edge;
-	// 바이러스의 연결 관계
-	static int[][] virus;
-	static boolean[] virustrue;
+    static int [][] relation;
+    static boolean [] visited;
 
-	public static void main(String[] args) throws Exception, Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static int result = 0;
 
-		// 컴퓨터의 수
-		Node = Integer.parseInt(br.readLine());
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		// 연결의 수
-		edge = Integer.parseInt(br.readLine());
+        N = Integer.parseInt(br.readLine());
 
-		// 바이러스 연결 관계
-		virus = new int[Node + 1][Node + 1];
-		
-		//바이러스가 걸렸는지 안걸렸는지
-		virustrue = new boolean[Node+1];
+        M = Integer.parseInt(br.readLine());
 
-		StringTokenizer st;
-		for (int i = 0; i < edge; i++) {
-			st = new StringTokenizer(br.readLine());
-			int start = Integer.parseInt(st.nextToken());
+        relation = new int[N+1][N+1];
+        visited = new boolean[N+1];
 
-			int end = Integer.parseInt(st.nextToken());
+        for(int i=0; i<M; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
-			virus[start][end] = 1;
-			virus[end][start] = 1;
-		}
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-		dfs(1);
-		
-		//1번 컴퓨터는 뺴야됨
-		System.out.println(cnt-1);
-	}
-	
-	//바이러스가 걸린 컴퓨터 수
-	static int cnt=0;
-	
-	static void dfs(int start) {
-		//일단 시작할 때 자기 자신은 바이러스가 걸림
-		virustrue[start] = true;
-		cnt++;
-		
-		for(int i=1; i<Node+1; i++) {
-			//연결이 되어있거나 방문을 안한 컴퓨터를 센다.
-			if(virus[start][i] == 1 && !virustrue[i]) {
-				dfs(i);
-			}
-		}
-	}
+            relation[start][end] = relation[end][start] = 1;
+        }
 
+
+        dfs(1);
+
+
+        System.out.println(result);
+    }
+
+    public static void dfs(int start){
+
+        visited[start] = true;
+        
+        for(int i=1; i<=N; i++){
+            if(relation[start][i] == 1 && !visited[i]){
+                result++;
+                dfs(i);
+            }
+        }
+
+    }
 }
