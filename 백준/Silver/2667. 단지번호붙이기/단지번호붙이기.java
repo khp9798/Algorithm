8fa29ea19310694011307630ac0b0e4200b2f0a4
khp9798 <121469544+khp9798@users.noreply.github.com>
2025-03-11 20:43:34 +0900

    [Silver I] Title: 단지번호붙이기, Time: 108 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -1,78 +1,70 @@
+import java.io.*;
+import java.util.*;
+
+public class Main{
+
+    static int N;
+
+    static int [][] map;
+
+    static boolean [][] visited;
+
+    static List<Integer> countarr = new ArrayList<>();
+
+    static int count;
+
+    static int [] dr = {-1,1,0,0};
+    static int [] dc = {0,0,-1,1};
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for(int i=0; i<N; i++){
+            String str = br.readLine();
+
+            for(int j=0; j<N; j++){
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+        for(int r=0; r<N; r++){
+            for(int c=0; c<N; c++){
+                if(map[r][c]==1 && !visited[r][c]){
+                    count=0;
+                    dfs(r,c);
+                    countarr.add(count);
+                }
+            }
+        }
 
-public class Main {
+        System.out.println(countarr.size());
+        Collections.sort(countarr);
+        for(int count : countarr){
+            System.out.println(count);
+        }
+    }
 
-	static int N;
-	static int[][] map;
-	static boolean[][] visited;
-	
-	static int[] dr = {-1,1,0,0};
-	static int[] dc = {0,0,-1,1};
-	
-	static int cnt=1;
-	
-	static List<Integer> answer = new ArrayList<>();
-	
-	public static void main(String[] args) throws Exception, Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		//지도의 크기
-		N = Integer.parseInt(br.readLine());
-		
-		//지도 내용 입력 받기
-		map = new int[N][N];
-		visited = new boolean[N][N];
-		
-		for(int r=0; r<N; r++) {
-			String[] str = br.readLine().split("");
-			for(int c=0; c<N; c++) {
-				map[r][c] = Integer.parseInt(str[c]);
-			}
-		}
-		
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<N; c++) {
-				if(map[r][c] == 1 && !visited[r][c]) {
-					dfs(r,c);
-					answer.add(cnt);
-					cnt=1;
-				}
-			}
-		}
-		
-		Collections.sort(answer);
-		
-		System.out.println(answer.size());
-		for(int a : answer) {
-			System.out.println(a);
-		}
 
-	}
-	
+    public static void dfs(int startR, int startC){
+        visited[startR][startC] = true;
 
-	
-	static void dfs(int r, int c) {
-		//일단 자기자신은 방문했으니까
-		visited[r][c] = true;
-		
-		//상하좌우 탐색
-		for(int d=0; d<4; d++) {
-			
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			
-			if(0<=nr && nr<N && 0<=nc && nc<N && !visited[nr][nc] && map[nr][nc]!=0) {
-				dfs(nr,nc);
-				cnt++;
-			}
-		}
-	}
+        count++;
 
+        for(int d=0; d<4; d++){
+            int nr = dr[d]+startR;
+            int nc = dc[d]+startC;
+
+            if(nr<0 || nr>=N || nc<0 || nc>=N)
+                continue;
+
+            if(map[nr][nc]==1 && !visited[nr][nc])
+                dfs(nr,nc);
+        }
+
+    }
 }
