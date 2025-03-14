a2e78be7b035de992b36e98b16819a8c2bd7735f
khp9798 <121469544+khp9798@users.noreply.github.com>
2025-03-15 00:15:41 +0900

    [Silver III] Title: N과 M (2), Time: 124 ms, Memory: 15764 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,55 +1,56 @@
+import java.io.*;
+import java.util.*;
 
+public class Main{
 
-import java.util.Scanner;
+    static int N;
+    static int M;
 
-public class Main {
+    static int [] result;
 
-	
-	//1부터 n까지 자연수 중에서 중복없이 m개를 고른 수열
-	//고른 수열은 오름차순
-	
-	static int N;
-	static int M;
-	static boolean [] visited;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+    static boolean [] visited;
+    public static void main(String[] args) throws IOException{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		
-		N = sc.nextInt();
-		M = sc.nextInt();
-		
-		visited = new boolean[N+1];
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-		
-		sunyul(1,0);
-	}
-	
-	
-	static void sunyul(int start, int cnt) {
-		
-		
-		if(cnt>=M) {
-			for(int i=1; i<=N; i++) {
-				if(visited[i])
-					System.out.print(i+" ");
-			}
-			System.out.println();
-			return;
-		}
-		
-		if(start>N) {
-			return; 
-		}
-		
-		
-		visited[start] = true;
-		sunyul(start+1,cnt+1);
-		visited[start] = false;
-		sunyul(start+1,cnt);
-		
-		
-	}
-	
-	
+        //1~N까지의 수
+        N = Integer.parseInt(st.nextToken());
 
+        //중복 없이 M개 고름
+        M = Integer.parseInt(st.nextToken());
+
+        
+        //중복되는 수열을 여러번 출력 x, 각 수열은 공백으로 구분해서 출력
+        //수열은 사전 순으로 증가하는 순서로 출력
+
+        visited = new boolean[N+1];
+
+        result = new int[M];
+
+        dfs(1,0);
+
+    }
+
+    public static void dfs(int num, int depth){
+
+        if(depth == M){
+            for (int res : result){
+                System.out.print(res+" ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for(int i=num; i<=N; i++){
+            if(!visited[i]){
+                visited[i] = true;
+                result[depth] = i;
+                dfs(i+1,depth+1);
+                visited[i] = false;
+            }
+        }
+
+    }
 }
