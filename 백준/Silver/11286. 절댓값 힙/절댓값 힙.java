f1e6d6ac5e1de141d9fc88f24a101dc6bb7e3de8
Hyeong Pyo Kim <121469544+khp9798@users.noreply.github.com>
2025-06-05 13:33:43 +0900

    [Silver I] Title: 절댓값 힙, Time: 1068 ms, Memory: 199304 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+			int x = Integer.parseInt(o1.toString());
+			int y = Integer.parseInt(o2.toString());
+
+			if (Math.abs(x) < Math.abs(y)) {
+				return -1;
+			} else if (Math.abs(x) == Math.abs(y)) {
+				return x - y;
+			} else {
+				return 1;
+			}
+		});
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		StringBuilder sb = new StringBuilder();
+
+		for (int i = 0; i < n; i++) {
+			int num = sc.nextInt();
+
+			if (num != 0) {
+				pq.add(num);
+			} else {
+				if (pq.isEmpty()) {
+					sb.append(0).append("\n");
+				} else {
+					sb.append(pq.poll() + "\n");
+				}
+
+			}
+		}
+
+		System.out.println(sb.toString());
+	}
+
+}
