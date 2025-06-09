549648dc3d4d46d04cc2efa8d74982f6b134b71a
Hyeong Pyo Kim <121469544+khp9798@users.noreply.github.com>
2025-06-09 16:23:31 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.70 ms, Memory: 89.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int solution(double num1, double num2) {
+        int answer = 1000;
+        double answer2 = num1/num2;
+        System.out.println(answer2);
+        answer*=answer2;
+        return answer;
+    }
+}
