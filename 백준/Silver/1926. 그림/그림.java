1c275baab065e60fec2b5db9191f1c53a54bf7ee
Hyeong Pyo Kim <121469544+khp9798@users.noreply.github.com>
2025-06-11 15:25:47 +0900

    [Silver I] Title: 그림, Time: 332 ms, Memory: 45168 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,75 +1,78 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int [][] map;
-	static boolean [][] visited;
-	static int N;
-	static int M;
-	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		//행의 길이
-		N = sc.nextInt();
-		//열의 길이
-		M = sc.nextInt();
-		
-		map = new int[N][M];
-		visited = new boolean[N][M];
-		
-		
-		
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<M; c++) {
-				map[r][c] = sc.nextInt();
+	static int[][] board;
+	static boolean[][] vis;
+	static int[] dr = { -1, 1, 0, 0 };
+	static int[] dc = { 0, 0, -1, 1 };
+	static int n, m;
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		// 가로 세로 크기
+		n = Integer.parseInt(st.nextToken());
+
+		m = Integer.parseInt(st.nextToken());
+
+		board = new int[n][m];
+		vis = new boolean[n][m];
+
+		for (int r = 0; r < n; r++) {
+			st = new StringTokenizer(br.readLine());
+			for (int c = 0; c < m; c++) {
+				board[r][c] = Integer.parseInt(st.nextToken());
 			}
 		}
-		
-		
-		int picture = 0;
-		for(int r=0; r<N; r++) {
-			for(int c=0; c<M; c++) {
-				if(map[r][c] == 1 && !visited[r][c]) {
-					dfs(r,c, 1);
-					picture++;
-					max = Math.max(max, picturecnt);
-					picturecnt = 1;
+
+		int count = 0;
+		int area = 0;
+		for (int r = 0; r < n; r++) {
+			for (int c = 0; c < m; c++) {
+				if (!vis[r][c] && board[r][c] == 1) {
+					count++;
+					area = Math.max(bfs(r, c), area);
 				}
 			}
 		}
-		
-		System.out.println(picture);
-		System.out.println(max);
+
+		System.out.println(count);
+		System.out.println(area);
 
 	}
 
-	static int [] dr = {-1,1,0,0};
-	static int [] dc = {0,0,-1,1};
-	
-	static int max = 0;
-	static int picturecnt = 1;
-	static void dfs(int r, int c, int cnt) {
-		visited[r][c] = true;
-		
-		for(int d=0; d<4; d++) {
-			int nr = r+dr[d];
-			int nc = c+dc[d];
-			
-			if(0<=nr && nr<N && 0<=nc && nc<M && map[nr][nc]==1 && !visited[nr][nc]) {
-				picturecnt++;
-				dfs(nr,nc,cnt+1);
+	static int bfs(int r, int c) {
+		Queue<int[]> q = new LinkedList<>();
+		vis[r][c] = true;
+		q.add(new int[] { r, c });
+
+		int area = 0;
+
+		while (!q.isEmpty()) {
+			int[] rc = q.poll();
+			int curR = rc[0];
+			int curC = rc[1];
+
+			area++;
+
+			for (int d = 0; d < 4; d++) {
+				int nr = curR + dr[d];
+				int nc = curC + dc[d];
+
+				if (nr < 0 || nc < 0 || nr >= n || nc >= m)
+					continue;
+				if (vis[nr][nc] || board[nr][nc] != 1)
+					continue;
+
+				vis[nr][nc] = true;
+				q.add(new int[] { nr, nc });
 			}
 		}
-		
+
+		return area;
 	}
-
-	
-	
-	
-	
-	
-
 }
