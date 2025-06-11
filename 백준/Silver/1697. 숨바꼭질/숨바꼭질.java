31d973a8092a27f6f7f44cc948fbedb4169358f8
Hyeong Pyo Kim <121469544+khp9798@users.noreply.github.com>
2025-06-11 17:14:14 +0900

    [Silver I] Title: 숨바꼭질, Time: 152 ms, Memory: 28356 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,77 +1,51 @@
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	static int [] road = new int[100001];
-	static boolean [] visited = new boolean[100001];
-	static class soobin{
-		int x;
-		int cnt;
-		public soobin(int x, int cnt) {
-			super();
-			this.x = x;
-			this.cnt = cnt;
-		}
-		
-		
-	}
-	public static void main(String[] args) throws Exception {
-		
+	static Queue<Integer> q = new LinkedList<>();
+	static int[] dist;
+
+	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
+
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		//수빈이 위치
-		int p1 = Integer.parseInt(st.nextToken());
-		
-		//동생 위치
-		int p2 = Integer.parseInt(st.nextToken());
-		
-		
-		bfs(p1,p2,0);
-		
-		System.out.println(result);
-	}
-	
-	static int result = 0;
-	static void bfs(int start, int end,int cnt) {
-		Queue<soobin> queue = new LinkedList<>();
-		queue.add(new soobin(start,cnt));
-		visited[start] = true;
-		
-		while(!queue.isEmpty()) {
-			//현재 위치
-			soobin sb = queue.poll();
-			
-			
-			if(sb.x==end) {
-				result = sb.cnt;
+
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+
+		dist = new int[200000];
+
+		q.add(n);
+		Arrays.fill(dist, -1);
+		dist[n] = 0;
+
+		while (!q.isEmpty()) {
+			int curr = q.poll();
+
+			if (curr == k) {
+				System.out.println(dist[curr]);
 				return;
 			}
-			
-			if(sb.x+1 < 100001 && !visited[sb.x+1]) {
-				visited[sb.x+1] = true;
-				queue.add(new soobin(sb.x+1,sb.cnt+1));				
+
+			int[] nx = new int[] { curr + 1, curr - 1, curr * 2 };
+
+			for (int num : nx) {
+				addQ(num, dist[curr]);
 			}
-			if(sb.x-1 >=0 && !visited[sb.x-1]) {
-				visited[sb.x-1] = true;
-				queue.add(new soobin(sb.x-1,sb.cnt+1));
-				
-			}
-			
-			if(sb.x*2 <100001 && !visited[sb.x*2]) {
-				visited[sb.x*2] = true;
-				queue.add(new soobin(sb.x*2,sb.cnt+1));				
-			}
-			
 		}
-		
+
+	}
+
+	static void addQ(int idx, int count) {
+		if (idx < 0 || idx >= 200000)
+			return;
+		else if (dist[idx] >= 0)
+			return;
+
+		dist[idx] = count + 1;
+		q.add(idx);
 	}
 
 }
