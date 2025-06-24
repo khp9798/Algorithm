b4a282e6f156c4e782eedecb6c6bb6f06387a5e9
김형표 <121469544+khp9798@users.noreply.github.com>
2025-06-24 18:13:46 +0900

    [Gold V] Title: 탑, Time: 748 ms, Memory: 167408 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -6,40 +6,42 @@ import java.io.OutputStreamWriter;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
-class Main{
-    public static void main(String[] args) throws IOException{
+class Main {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int n = Integer.parseInt(br.readLine());
 
-        int [] top = new int[n];
+        int[] top = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             top[i] = Integer.parseInt(st.nextToken());
         }
 
-        int [] res = new int[n];
+        int[] res = new int[n];
 
         Stack<Integer> stack = new Stack<>();
 
-        for(int i=0; i<n; i++){
-            
-            while(!stack.isEmpty() && top[stack.peek()]<top[i]){
+        for (int i = 0; i < n; i++) {
+
+            while (!stack.isEmpty() && top[stack.peek()] < top[i]) {
                 stack.pop();
             }
 
-            if(!stack.isEmpty()){
-                res[i] = stack.peek()+1;
+            if (!stack.isEmpty()) {
+                res[i] = stack.peek() + 1;
             }
             stack.push(i);
         }
 
-        for(int i=0; i<n; i++){
-            bw.write(res[i]+" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            sb.append(res[i] + " ");
         }
 
+        bw.write(sb.toString());
         bw.flush();
     }
 }
