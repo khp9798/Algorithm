cf9259e770f7728be4c96b409d202f7fa3cc8314
김형표 <121469544+khp9798@users.noreply.github.com>
2025-07-02 17:12:29 +0900

    [Gold V] Title: 최소비용 구하기, Time: 460 ms, Memory: 52536 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.java
@@ -1,96 +1,87 @@
-import java.util.*;
+
 import java.io.*;
+import java.util.*;
 
 class Main {
-	static class bus implements Comparable<bus> {
-		int to;
-		int W;
 
-		public bus(int to, int w) {
-			super();
-			this.to = to;
-			W = w;
-		}
+    static class City implements Comparable<City> {
 
-		@Override
-		public int compareTo(bus o) {
-			// TODO Auto-generated method stub
-			return this.W - o.W;
-		}
+        int to, cost;
 
-		@Override
-		public String toString() {
-			return "bus [to=" + to + ", W=" + W + "]";
-		}
+        City(int to, int cost) {
+            this.to = to;
+            this.cost = cost;
+        }
 
-	}
+        @Override
+        public int compareTo(City o) {
+            return this.cost - o.cost;
+        }
 
-	static int N;
-	static int M;
-	static List<bus>[] bus;
+    }
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
+        // 코드 작성
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
 
-		N = Integer.parseInt(br.readLine());
-		M = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
 
-		bus = new ArrayList[N + 1];
-		for (int i = 0; i <= N; i++) {
-			bus[i] = new ArrayList<>();
-		}
-		StringTokenizer st;
-		for (int i = 0; i < M; i++) {
-			st = new StringTokenizer(br.readLine());
+            int m = Integer.parseInt(br.readLine());
 
-			int from = Integer.parseInt(st.nextToken());
-			int to = Integer.parseInt(st.nextToken());
-			int W = Integer.parseInt(st.nextToken());
+            List<City>[] graph = new ArrayList[n + 1];
 
-			bus[from].add(new bus(to, W));
-		}
+            for (int i = 1; i <= n; i++) {
+                graph[i] = new ArrayList<>();
+            }
 
-		int[] dist = new int[N + 1];
-		boolean[] visited = new boolean[N + 1];
+            StringTokenizer st;
+            for (int i = 0; i < m; i++) {
+                st = new StringTokenizer(br.readLine());
 
-		st = new StringTokenizer(br.readLine());
+                int sc = Integer.parseInt(st.nextToken());
 
-		int start = Integer.parseInt(st.nextToken());
-		int dest = Integer.parseInt(st.nextToken());
+                int ec = Integer.parseInt(st.nextToken());
 
-		Arrays.fill(dist, Integer.MAX_VALUE);
+                int cost = Integer.parseInt(st.nextToken());
 
-		dist[start] = 0;
+                graph[sc].add(new City(ec, cost));
+            }
 
-		PriorityQueue<bus> pq = new PriorityQueue<>();
+            st = new StringTokenizer(br.readLine());
 
-		pq.add(new bus(start, 0));
-//		visited[start] = true;
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
 
-		while (!pq.isEmpty()) {
-			bus currbus = pq.poll();
-			int curr = currbus.to;
-			int W = currbus.W;
+            int[] dist = new int[n + 1];
 
-			if (visited[curr])
-				continue;
-			visited[curr] = true;
+            int INF = Integer.MAX_VALUE;
 
-			for (bus bus1 : bus[curr]) {
-				int nextTo = bus1.to;
-				int nextdist = dist[curr] + bus1.W;
+            Arrays.fill(dist, INF);
 
-				if (nextdist < dist[nextTo]) {
-					dist[nextTo] = nextdist;
-					pq.add(new bus(nextTo,nextdist));
-				}
+            dist[start] = 0;
 
-				
-			}
+            PriorityQueue<City> pq = new PriorityQueue<>();
 
-		}
+            pq.offer(new City(start, 0));
 
-		System.out.println(dist[dest]);
+            boolean[] gone = new boolean[n + 1];
+            while (!pq.isEmpty()) {
+                City cur = pq.poll();
+                if (gone[cur.to]) {
+                    continue;
+                }
+                gone[cur.to] = true;
 
-	}
+                for (City c : graph[cur.to]) {
+                    if (dist[c.to] > dist[cur.to] + c.cost) {
+                        dist[c.to] = dist[cur.to] + c.cost;
+                        pq.offer(new City(c.to, dist[c.to]));
+                    }
+                }
+            }
+
+            System.out.println(dist[end]);
+        }
+    }
+
 }
