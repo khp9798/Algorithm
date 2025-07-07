d81177da018a39884bb9dcfbdfe03d8e3ae5a813
김형표 <121469544+khp9798@users.noreply.github.com>
2025-07-07 11:43:39 +0900

    [Silver III] Title: Four Squares, Time: 92 ms, Memory: 12908 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.java
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -7,28 +7,28 @@ class Main {
 
         int n = sc.nextInt();
 
-        int c1 = (int)Math.sqrt(n);
-        if(c1*c1 == n){
+        int c1 = (int) Math.sqrt(n);
+        if (c1 * c1 == n) {
             System.out.println(1);
             return;
         }
 
-
-        for(int i=1; i<=(int)Math.sqrt(n); i++){
-            int num = n-i*i;
+        for (int i = 1; i <= (int) Math.sqrt(n); i++) {
+            int num = n - i * i;
             int c2 = (int) Math.sqrt(num);
-            if(c2*c2 == num){
+            if (c2 * c2 == num) {
                 System.out.println(2);
                 return;
             }
         }
 
-        int m= n;
-        while(m%4 == 0){
-            m/=4;
+        while (n % 4 == 0) {
+            n /= 4;
         }
 
-        if(m%8 == 7) System.out.println(4);
-        else System.out.println(3);
+        if (n % 8 == 7)
+            System.out.println(4);
+        else
+            System.out.println(3);
     }
 }
