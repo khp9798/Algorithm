4cc1cdbc8ebecc665eb770db992040f494963b55
Hyeong Pyo Kim <121469544+khp9798@users.noreply.github.com>
2025-08-08 16:01:36 +0900

    [Silver III] Title: 퇴사, Time: 96 ms, Memory: 12900 KB -BaekjoonHub

---
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[] D = new int[N + 2]; // 오늘부터 퇴사일까지 벌 수 있는 최대 수입
+        int[] T = new int[N + 1];
+        int[] P = new int[N + 1];
+        for(int i = 1; i <= N; i++)
+        {
+        T[i] = sc.nextInt();
+        P[i] = sc.nextInt();
+        }
+        for(int i = N; i > 0; i--) {
+            if(i + T[i] > N + 1) {
+                D[i] = D[i + 1];
+            }
+            else {
+            D[i] = Math.max(D[i + 1], P[i] + D[i + T[i]]);
+            }
+        }
+    System.out.println(D[1]);
+    }
+}
