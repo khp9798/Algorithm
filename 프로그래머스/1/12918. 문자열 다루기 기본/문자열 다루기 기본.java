4d940958a005a56c2ebff24996e07342394293ca
Hyeong Pyo Kim <121469544+khp9798@users.noreply.github.com>
2025-10-01 14:23:04 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.08 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -1,14 +1,14 @@
+
 class Solution {
     public boolean solution(String s) {
         boolean answer = true;
         
         if(s.length() != 4 && s.length() != 6){
-            System.out.println("문자열의 길이 : "+s.length());
             return false;  
         }
         
         for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)-'0' < 0 || s.charAt(i)-'0' > 9){
+            if(!Character.isDigit(s.charAt(i))){
                 return false;
             }
         }
