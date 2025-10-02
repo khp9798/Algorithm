f35873279ce3b216b2e0ba15ceb2cb54c1dd641e
Hyeong Pyo Kim <121469544+khp9798@users.noreply.github.com>
2025-10-02 15:24:46 +0900

    [level 1] Title: K번째수, Time: 0.50 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -5,18 +5,21 @@ class Solution {
         int[] answer = new int[commands.length];
         
         for(int r=0; r<commands.length; r++){
+            int i = commands[r][0];
+            int j = commands[r][1];
+            int k = commands[r][2];
             
-            int[] subarr = Arrays.copyOfRange(array,commands[r][0]-1,commands[r][1]);
-            
-            Arrays.sort(subarr);
-            answer[r] = subarr[commands[r][2]-1];
-            
-            for(int i=0; i<subarr.length; i++){
-                System.out.print(subarr[i]+" ");
-            }
-            
-            System.out.println();
+            answer[r] = cal(array,i,j,k);
         }
+        
         return answer;
+    }
+    
+    public int cal(int[] arr, int i, int j, int k){
+        int[] temp = Arrays.copyOfRange(arr,i-1,j);
+        
+        Arrays.sort(temp);
+    
+        return temp[k-1];
     }
 }
