91b40e1ddab3e49160ca6c877210d8f813f59422
Hyeong Pyo Kim <121469544+khp9798@users.noreply.github.com>
2025-10-10 17:48:42 +0900

    [level 1] Title: 모의고사, Time: 3.37 ms, Memory: 91.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] answers) {
+        int[] one   = {1,2,3,4,5};
+        int[] two   = {2,1,2,3,2,4,2,5};
+        int[] three = {3,3,1,1,2,2,4,4,5,5};
+
+        int[] score = new int[3];
+
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == one[i % one.length])   score[0]++;
+            if (answers[i] == two[i % two.length])   score[1]++;
+            if (answers[i] == three[i % three.length]) score[2]++;
+        }
+
+        int max = Math.max(score[0], Math.max(score[1], score[2]));
+
+        List<Integer> res = new ArrayList<>();
+        for (int i = 0; i < 3; i++) if (score[i] == max) res.add(i + 1);
+
+        return res.stream().mapToInt(Integer::intValue).toArray();
+    }
+}
